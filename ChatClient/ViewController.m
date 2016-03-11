9c31927fa48dc217a1671c72dc04062d722f1905
beepscore <support@beepscore.com>
2016-03-11 08:39:43 -0800

    Add method initNetworkCommunication

--- ChatClient/ViewController.m
+++ ChatClient/ViewController.m
@@ -35,4 +35,32 @@
 
 - (IBAction)joinChat:(id)sender {
 }
+
+#pragma mark - stream methods
+
+/**
+ * Use toll free bridge between NSStream and CFStream
+ * Technical Q&A QA1652
+ * Using NSStreams For A TCP Connection Without NSHost
+ * shows using CFBridgingRelease
+ * https://developer.apple.com/library/ios/qa/qa1652/_index.html
+ */
+- (void)initNetworkCommunication {
+    // bind CFStreams to a host and a port
+    CFReadStreamRef readStream;
+    CFWriteStreamRef writeStream;
+    UInt32 port = 80;
+    CFStreamCreatePairWithSocketToHost(NULL, (CFStringRef)@"localhost",
+                                       port,
+                                       &readStream, &writeStream);
+    
+    // Now bridged cast the CFStreams to NSStreams
+    // Xcode fixit said needed to bridge, offered 2 solutions
+    // use __bridge instead of CFBridgingRelease
+    // Not sure which choice is more correct
+    // http://stackoverflow.com/questions/18067108/when-should-you-use-bridge-vs-cfbridgingrelease-cfbridgingretain
+    inputStream = (__bridge NSInputStream *)readStream;
+    outputStream = (__bridge NSOutputStream *)writeStream;
+}
+
 @end
