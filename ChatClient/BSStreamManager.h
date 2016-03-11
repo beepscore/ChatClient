6eb4f9866addbc6f81b54dd0827cf9696bc6f974
beepscore <support@beepscore.com>
2016-03-11 12:55:27 -0800

    Add class BSStreamManager. TODO: Extract stream management code from view controller into this class.

---
+++ ChatClient/BSStreamManager.h
@@ -0,0 +1,13 @@
+//
+//  BSStreamManager.h
+//  ChatClient
+//
+//  Created by Steve Baker on 3/11/16.
+//  Copyright © 2016 Beepscore LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BSStreamManager : NSObject
+
+@end
---
+++ ChatClient/BSStreamManager.m
@@ -0,0 +1,13 @@
+//
+//  BSStreamManager.m
+//  ChatClient
+//
+//  Created by Steve Baker on 3/11/16.
+//  Copyright © 2016 Beepscore LLC. All rights reserved.
+//
+
+#import "BSStreamManager.h"
+
+@implementation BSStreamManager
+
+@end
